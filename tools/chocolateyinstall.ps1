d2c4cfd577c0fb9331ad469952b322994668b50c
Tyler McAdams <tyler.mcadams@gmail.com>
2019-09-06 17:39:39 -0400

    ctm - add remote package download

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
- 
-$fileName  = 'schily-cdrtools-3.02a07.7z'
+
+$version = "3.02a07"
+$url = "https://svwh.dl.sourceforge.net/project/tumagcc/schily-cdrtools-$version.7z"
+$checksum = "BAD39F9B5CEF30013E8DEF5375397C3B891745BC4C69B30532908B3A89116121"
+$fileName  = "schily-cdrtools-$version.7z"
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
  
 $packageArgs = @{
-    PackageName  = 'schily-cdrtools'
-    FileFullPath = $zip_path
-    Destination  = $toolsPath
+    Url             = $url
+    Checksum        = $checksum
+    ChecksumType    = 'sha256'
+    PackageName     = 'schily-cdrtools'
+    FileFullPath    = $zip_path
+    Destination     = $toolsPath
 }
 Get-ChocolateyUnzip @packageArgs
 Remove-Item $zip_path -ea 0
