8aa15d339e43d6d67f265292b98ad8615f68da48
Tyler McAdams <tyler.mcadams@gmail.com>
2019-09-05 22:58:17 -0400

    ctm - initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$fileName  = 'schily-cdrtools-3.02a07.7z'
+$toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$zip_path = "$toolsPath\$fileName"
+Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
+ 
+$packageArgs = @{
+    PackageName  = 'schily-cdrtools'
+    FileFullPath = $zip_path
+    Destination  = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $zip_path -ea 0
